d12f280e06668c0ae769c5dcc17b6356d3d235d6
ormam <36844889+ormam@users.noreply.github.com>
2018-05-21 02:53:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello TrainLogic!!!\n');
+  res.send('Hello IBM!!!\n');
 });
 
 app.listen(PORT, HOST);
